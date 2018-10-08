e160b997bac59317a45b84dcc7b8cb41ecd6dc83
klcatha <klcatha0518@gmail.com>
2018-10-08 15:32:32 +0900

    search button

--- app/src/main/java/com/example/kengomaruyama/qiitaclient/MainActivity.kt
+++ app/src/main/java/com/example/kengomaruyama/qiitaclient/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.kengomaruyama.qiitaclient
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ListView
 import com.example.kengomaruyama.qiitaclient.model.Article
 import com.example.kengomaruyama.qiitaclient.model.User
 import com.example.kengomaruyama.qiitaclient.view.ArticleView
+import com.google.gson.FieldNamingPolicy
+import com.google.gson.GsonBuilder
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import rx.android.schedulers.AndroidSchedulers
+import rx.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,8 +29,34 @@ class MainActivity : AppCompatActivity() {
         val listView: ListView = findViewById(R.id.list_view) as ListView
         listView.adapter = listAdapter
         listView.setOnItemClickListener{ adapterView, view, position, id ->
-            val article = listAdapter.articles[position]
-            ArticleActivity.intent(this, article).let { startActivity(it) }
+            val intent = ArticleActivity.intent(this, listAdapter.articles[position])
+            startActivity(intent)
+        }
+
+        val gson = GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+                .create()
+        val retrofit = Retrofit.Builder()
+                .baseUrl("http://qiita.com")
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .build()
+        val articleClient = retrofit.create(ArticleClient::class.java)
+
+        val queryEditText = findViewById(R.id.query_edit_text) as EditText
+        val searchButton = findViewById(R.id.search_button) as Button
+
+        searchButton.setOnClickListener {
+            articleClient.search(queryEditText.text.toString())
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe({
+                        queryEditText.text.clear()
+                        listAdapter.articles = it
+                        listAdapter.notifyDataSetChanged()
+                    },{
+                        toast("エラー:$it")
+                    })
         }
     }
 
