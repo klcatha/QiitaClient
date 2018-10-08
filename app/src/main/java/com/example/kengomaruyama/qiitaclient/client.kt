322c66721b24a9036690a780487e3e9983b4eeac
klcatha <klcatha0518@gmail.com>
2018-10-08 15:33:51 +0900

    add client

--- app/src/main/java/com/example/kengomaruyama/qiitaclient/client.kt
+++ app/src/main/java/com/example/kengomaruyama/qiitaclient/client.kt
@@ -1,2 +1,12 @@
 package com.example.kengomaruyama.qiitaclient
 
+import retrofit2.http.GET
+import retrofit2.http.Query
+import rx.Observable
+import com.example.kengomaruyama.qiitaclient.model.Article
+
+interface ArticleClient {
+
+    @GET("/api/v2/items")
+    fun search(@Query("query") query: String): Observable<List<Article>>
+}
