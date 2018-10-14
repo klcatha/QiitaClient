451d42c9cc6ac17620dd36fc2fba9978eb22bb62
klcatha <klcatha0518@gmail.com>
2018-10-14 09:50:46 +0900

    add test code

--- app/src/androidTest/java/com/example/kengomaruyama/qiitaclient/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/kengomaruyama/qiitaclient/MainActivityTest.kt
@@ -1,2 +1,14 @@
 package com.example.kengomaruyama.qiitaclient
 
+import android.support.test.runner.AndroidJUnit4
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.lang.AssertionError
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest{
+    @Test
+    fun test(){
+        throw AssertionError("失敗するはず")
+    }
+}
