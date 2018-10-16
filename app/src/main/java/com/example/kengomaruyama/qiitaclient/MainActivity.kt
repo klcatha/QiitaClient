b41399cb30383d899785a8290763d964fcd7e8dd
klcatha <klcatha0518@gmail.com>
2018-10-16 10:20:09 +0900

    fix

--- app/src/main/java/com/example/kengomaruyama/qiitaclient/MainActivity.kt
+++ app/src/main/java/com/example/kengomaruyama/qiitaclient/MainActivity.kt
@@ -19,35 +19,29 @@ import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import rx.android.schedulers.AndroidSchedulers
 import rx.schedulers.Schedulers
+import javax.inject.Inject
 
 class MainActivity : RxAppCompatActivity() {
 
+    @Inject
+    lateinit var articleClient: ArticleClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        (application as QiitaClientApp).component.inject(this)
         setContentView(R.layout.activity_main)
 
-        val listAdapter = ArticleListAdapter(applicationContext)
-
         val listView: ListView = findViewById(R.id.list_view) as ListView
+        val progressBar = findViewById(R.id.progress_bar) as ProgressBar
+        val queryEditText = findViewById(R.id.query_edit_text) as EditText
+        val searchButton = findViewById(R.id.search_button) as Button
+
+        val listAdapter = ArticleListAdapter(applicationContext)
         listView.adapter = listAdapter
         listView.setOnItemClickListener{ adapterView, view, position, id ->
             val intent = ArticleActivity.intent(this, listAdapter.articles[position])
             startActivity(intent)
         }
-        val progressBar = findViewById(R.id.progress_bar) as ProgressBar
-
-        val gson = GsonBuilder()
-                .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
-                .create()
-        val retrofit = Retrofit.Builder()
-                .baseUrl("http://qiita.com")
-                .addConverterFactory(GsonConverterFactory.create(gson))
-                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
-                .build()
-        val articleClient = retrofit.create(ArticleClient::class.java)
-
-        val queryEditText = findViewById(R.id.query_edit_text) as EditText
-        val searchButton = findViewById(R.id.search_button) as Button
 
         searchButton.setOnClickListener {
             progressBar.visibility = View.VISIBLE
